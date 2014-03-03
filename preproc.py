51e41267567af34bbc2626e341c9773220abbf90
DallasTrinkle <dtrinkle@illinois.edu>
2014-03-03 17:42:36 -0600

    Added preproc script, and changes for multiple answer support, raw-scan PDF feedback, .signature in feedback email, and modified documentation accordingly
    1. preproc.py script takes a tex file, and expands out all of the \input; this is useful for writing an exam.tex file that uses \input to import questions from a question bank, and produces a library file that can be used for randexam.
    
    2. Changed randexam to support multiple answers per question from students, and partial credit. Introduces a "bubble" array that encodes all of the answers that a student has selected, and then grade / statistic / feedback accordingly.
    
    3. Added support for a rawscan directory that includes PDFs of individual exams scanned in, that will be sent as an additional attachment to students in the feedback processing.
    
    4. Added support to clarify the msg_txt for the email, and add in the ~/.signature file to the message, if present.
    
    5. Edited documentation to include information about the multiple answer features and formatting of randexam.

---
+++ preproc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv)<2):
+        print "Usage: ", sys.argv[0], " texfile"
+        print """  texfile  texfile to preprocess
+
+Takes texfile and expands all \input commands.
+Requires that each \input line be of the format:
+\input filename.tex
+(That is: no braces, no missing .tex extension)
+"""
+        quit(1)
+
+    # we implement this with a stack; the top of which is the current file being read.
+    # each \input command pushes a new open file, and EOF pops.
+    # need to be careful about commented lines
+    F = [open(sys.argv[1])]
+    while len(F)>0:
+        line = F[-1].readline()
+        if line == "":
+            # EOF... so pop, and close
+            F.pop().close()
+        else:
+            # note: we do modulo arithmetic so that if -1 comes back (no match)
+            # then it returns length-1. So the only way this will work is:
+            # a) if there's \input
+            # b) and it's found before a % (or no % at all).
+            # Note that if we don't find \input, then it will automatically be >=
+            # to whereever we "find" a %.
+            n = line.find("\\input") % len(line)
+            m = line.find("%") % len(line)
+            if (n < m):
+                # new file! throw it on the stack; strip out \input and whitespace
+                F.append(open(line[n+7:m].strip()))
+            else:
+                # else, just print
+                print line,
